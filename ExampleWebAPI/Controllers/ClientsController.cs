ee42d0dcaf91ad1c34897bba39b83e8c40c2d6ef
Gabriel Casagrande <gabriel.casagrande@estudante.ufla.br>
2020-09-24 11:36:29 -0300

    :rocket: add first simple model/controller

---
+++ ExampleWebAPI/Controllers/ClientsController.cs
@@ -0,0 +1,42 @@
+﻿using ExemploWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Web.Http;
+
+namespace ExemploWebAPI.Controllers
+{
+    public class ClientsController : ApiController
+    {
+
+        private static List<Client> clientsList = new List<Client>();
+
+        [HttpGet]
+        public List<Client> listClients()
+        {
+            return clientsList;
+        }
+
+        [HttpPost]
+        public void insertClient(string name)
+        {
+            if (!string.IsNullOrEmpty(name))
+            {
+                clientsList.Add(new Client(name));
+            }
+
+        }
+
+        [HttpDelete]
+        public void removeClient(string name)
+        {
+            var idToDelete = clientsList.IndexOf(clientsList.First(x => x.Name.Equals(name)));
+
+            clientsList.RemoveAt(idToDelete);
+        }
+
+    }
+}
---
+++ ExampleWebAPI/Models/Client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExemploWebAPI.Models
+{
+    public class Client
+    {
+        public string Name { get; set; }
+
+        public Client(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
